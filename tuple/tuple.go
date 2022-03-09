13639c5c9eca94b0d3b40a38906506a4d4a9ec0d
Jesse Pasley <jessepasley@gmail.com>
2022-03-09 17:15:25 -0600

    Chapter 1

--- tuple/tuple.go
+++ tuple/tuple.go
@@ -1,15 +1,117 @@
 package tuple
 
+import "math"
+
 // import "math"
 
 type FourTuple struct {
-	Elems []float64
+	Elements []float64
 }
 
 func NewVector(x, y, z float64) *FourTuple {
 	return &FourTuple{[]float64{x, y, z, 0.0}}
 }
 
-func (this FourTuple) IsVector() bool {
-	return this.Elems[3] == 0.0
+func (t FourTuple) IsVector() bool {
+	return t.Elements[3] == 0.0
+}
+
+func NewPoint(x, y, z float64) *FourTuple {
+	return &FourTuple{[]float64{x, y, z, 1.0}}
+}
+
+func (t FourTuple) IsPoint() bool {
+	return t.Elements[3] == 1.0
+}
+
+func NewFourTuple(elements []float64) *FourTuple {
+	return &FourTuple{Elements: elements}
+}
+
+func (t FourTuple) Get(row int) float64 {
+	return t.Elements[row]
+}
+
+func Add(tup1, tup2 FourTuple) *FourTuple {
+	res := NewFourTuple(make([]float64, 4))
+	for i := range res.Elements {
+		res.Elements[i] = tup1.Get(i) + tup2.Get(i)
+	}
+	return res
+}
+
+func Subtract(tup1, tup2 FourTuple) *FourTuple {
+	res := NewFourTuple(make([]float64, 4))
+	for i := range res.Elements {
+		res.Elements[i] = tup1.Get(i) - tup2.Get(i)
+	}
+	return res
+}
+
+func Negate(tup FourTuple) *FourTuple {
+	res := NewFourTuple(make([]float64, 4))
+	for i := range res.Elements {
+		res.Elements[i] = 0 - tup.Get(i)
+	}
+	return res
+}
+
+func MultiplyByScalar(tup1 FourTuple, scalar float64) *FourTuple {
+	res := &FourTuple{Elements: make([]float64, 4)}
+	for i := 0; i < 4; i++ {
+		res.Elements[i] = tup1.Get(i) * scalar
+	}
+	return res
+}
+
+func DivideByScalar(tup1 FourTuple, scalar float64) *FourTuple {
+	res := &FourTuple{Elements: make([]float64, 4)}
+	for i := 0; i < 4; i++ {
+		res.Elements[i] = tup1.Get(i) / scalar
+	}
+	return res
+}
+
+func Magnitude(tup1 FourTuple) float64 {
+	return math.Sqrt(tup1.Elements[0]*tup1.Elements[0] +
+		tup1.Elements[1]*tup1.Elements[1] +
+		tup1.Elements[2]*tup1.Elements[2])
+}
+
+//NormalLize
+func Normalize(tup1 FourTuple) *FourTuple {
+	res := &FourTuple{Elements: make([]float64, 4)}
+	magnitude := Magnitude(tup1)
+	for i := 0; i < 4; i++ {
+		res.Elements[i] = tup1.Get(i) / magnitude
+	}
+	return res
+}
+
+//Dot
+func Dot(tup1 FourTuple, tup2 FourTuple) float64 {
+	sum := 0.0
+	for i := 0; i < 4; i++ {
+		sum += tup1.Get(i) * tup2.Get(i)
+	}
+	return sum
+}
+
+//Cross
+func Cross(tup1 FourTuple, tup2 FourTuple) *FourTuple {
+	res := &FourTuple{Elements: make([]float64, 4)}
+
+	res.Elements[0] = tup1.Get(1)*tup2.Get(2) - tup1.Get(2)*tup2.Get(1)
+	res.Elements[1] = tup1.Get(2)*tup2.Get(0) - tup1.Get(0)*tup2.Get(2)
+	res.Elements[2] = tup1.Get(0)*tup2.Get(1) - tup1.Get(1)*tup2.Get(0)
+	res.Elements[3] = 0
+	return res
+}
+
+//Eq
+func TupleEquals(tup1, tup2 FourTuple) bool {
+	return Eq(tup1.Get(0), tup2.Get(0)) &&
+		Eq(tup1.Get(1), tup2.Get(1)) &&
+		Eq(tup1.Get(2), tup2.Get(2)) &&
+		Eq(tup1.Get(3), tup2.Get(3))
 }
--- tuple/tuple_test.go
+++ tuple/tuple_test.go
@@ -1,13 +1,17 @@
 package tuple
 
 import (
+	"math"
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTuple_IsVector(t *testing.T) {
 
 	t.Run("is vector", func(t *testing.T) {
-		testVector := NewVector(4.3, -4.2, 3.1)
+		testVector := NewVector(4.2, -4.7, 1.4)
 		got := testVector.IsVector()
 		want := true
 
@@ -15,4 +19,153 @@ func TestTuple_IsVector(t *testing.T) {
 			t.Errorf(" this is not a vector")
 		}
 	})
+}
+
+func TestTuple_IsPoint(t *testing.T) {
+	t.Run("is point", func(t *testing.T) {
+		testPoint := NewPoint(4.2, -4.7, 1.4)
+		got := testPoint.IsPoint()
+		want := true
+
+		if got != want {
+			t.Errorf(" this is not a point")
+		}
+	})
+}
+
+func TestTuple_Add(t *testing.T) {
+	t.Run("add", func(t *testing.T) {
+		testPoint := NewPoint(1.0, 1.0, 1.0)
+		testVector := NewVector(2.0, 2.1, 2.2)
+		got := Add(*testPoint, *testVector)
+		want := NewFourTuple([]float64{3.0, 3.1, 3.2, 1.0})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf(" hmmm")
+		}
+	})
+}
+
+func TestTuple_Subtract(t *testing.T) {
+	t1 := NewPoint(7, 1, 0)
+	t2 := NewPoint(5, 6, 7)
+	t3 := Subtract(*t1, *t2)
+	assert.Equal(t, 2.0, t3.Get(0))
+	assert.Equal(t, -5.0, t3.Get(1))
+	assert.Equal(t, -7.0, t3.Get(2))
+	assert.Equal(t, 0.0, t3.Get(3))
+}
+
+func TestSubtractVectorFromPoint(t *testing.T) {
+	t1 := NewPoint(5, 1, 19)
+	t2 := NewVector(5, 8, 2)
+
+	t3 := Subtract(*t1, *t2)
+	assert.Equal(t, 0.0, t3.Get(0))
+	assert.Equal(t, -7.0, t3.Get(1))
+	assert.Equal(t, 17.0, t3.Get(2))
+	assert.Equal(t, 1.0, t3.Get(3))
+}
+
+func TestSubtractVectorFromVector(t *testing.T) {
+	t1 := NewVector(5, 1, 19)
+	t2 := NewVector(5, 8, 2)
+
+	res := Subtract(*t1, *t2)
+	assert.Equal(t, 0.0, res.Get(0))
+	assert.Equal(t, -7.0, res.Get(1))
+	assert.Equal(t, 17.0, res.Get(2))
+	assert.Equal(t, 0.0, res.Get(3))
+}
+
+func TestNegateTuple(t *testing.T) {
+	tup1 := FourTuple{[]float64{2, -1, 19, -4}}
+	res := Negate(tup1)
+
+	assert.Equal(t, -2.0, res.Get(0))
+	assert.Equal(t, 1.0, res.Get(1))
+	assert.Equal(t, -19.0, res.Get(2))
+	assert.Equal(t, 4.0, res.Get(3))
+}
+
+func TestMultiplyByScalar(t *testing.T) {
+	t1 := FourTuple{[]float64{1, -2, 3, -4}}
+	t3 := MultiplyByScalar(t1, 3.5)
+	assert.Equal(t, 3.5, t3.Get(0))
+	assert.Equal(t, -7.0, t3.Get(1))
+	assert.Equal(t, 10.5, t3.Get(2))
+	assert.Equal(t, -14.0, t3.Get(3))
+}
+
+func TestMultiplyByScalarFraction(t *testing.T) {
+	t1 := FourTuple{[]float64{1, -2, 3, -4}}
+	t3 := MultiplyByScalar(t1, 0.5)
+	assert.Equal(t, 0.5, t3.Get(0))
+	assert.Equal(t, -1.0, t3.Get(1))
+	assert.Equal(t, 1.5, t3.Get(2))
+	assert.Equal(t, -2.0, t3.Get(3))
+}
+
+func TestDivideByScalar(t *testing.T) {
+	t1 := FourTuple{[]float64{1, -2, 3, -4}}
+	t3 := DivideByScalar(t1, 2)
+	assert.Equal(t, 0.5, t3.Get(0))
+	assert.Equal(t, -1.0, t3.Get(1))
+	assert.Equal(t, 1.5, t3.Get(2))
+	assert.Equal(t, -2.0, t3.Get(3))
+}
+
+func TestMagnitude(t *testing.T) {
+	tc := []struct {
+		tpl *FourTuple
+		out float64
+	}{
+		{NewVector(1, 0, 0), 1.0},
+		{NewVector(0, 1, 0), 1.0},
+		{NewVector(0, 0, 1), 1.0},
+		{NewVector(1, 2, 3), math.Sqrt(14)},
+		{NewVector(-1, -2, -3), math.Sqrt(14)},
+	}
+
+	for _, test := range tc {
+		assert.Equal(t, test.out, Magnitude(*test.tpl))
+	}
+}
+
+func TestNormalizeXOnly(t *testing.T) {
+	t1 := NewVector(4, 0, 0)
+	t3 := Normalize(*t1)
+	assert.Equal(t, 1.0, t3.Get(0))
+	assert.Equal(t, 0.0, t3.Get(1))
+	assert.Equal(t, 0.0, t3.Get(2))
+}
+
+func TestNormalizeXYZ(t *testing.T) {
+	t1 := NewVector(1, 2, 3)
+	t3 := Normalize(*t1)
+	assert.True(t, Eq(0.26726, t3.Get(0)))
+	assert.True(t, Eq(0.53452, t3.Get(1)))
+	assert.True(t, Eq(0.80178, t3.Get(2)))
+}
+
+func TestNormalizedMagnitudeIsOne(t *testing.T) {
+	t1 := NewVector(1, 2, 3)
+	t3 := Normalize(*t1)
+	assert.Equal(t, 1.0, Magnitude(*t3))
+}
+
+func TestDot(t *testing.T) {
+	t1 := NewVector(1, 2, 3)
+	t2 := NewVector(2, 3, 4)
+	dotProduct := Dot(*t1, *t2)
+	assert.Equal(t, 20.0, dotProduct)
+}
+
+func TestCross(t *testing.T) {
+	t1 := NewVector(1, 2, 3)
+	t2 := NewVector(2, 3, 4)
+	crossT1 := Cross(*t1, *t2)
+	crossT2 := Cross(*t2, *t1)
+	assert.True(t, TupleEquals(*crossT1, *NewVector(-1, 2, -1)))
+	assert.True(t, TupleEquals(*crossT2, *NewVector(1, -2, 1)))
 }
